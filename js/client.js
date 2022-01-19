20233bcc3e46dbc4f96be1ed93f7d348026c2daf
TRUP02 <96164595+TRUP02@users.noreply.github.com>
2022-01-19 17:44:09 +0530

    chat_app
    bakend mistaken here

---
+++ js/client.js
@@ -0,0 +1,9 @@
+
+const socket = io("https://localhost:8000");
+
+const form = document.getElementById('send');
+const messageInput = document.getElementById('sendip');
+const messageContainer = document.querySelector('.container');
+
+
+const name = prompt('Enter your name ');
